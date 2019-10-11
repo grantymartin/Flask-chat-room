44acf9f442d490cf262e934def8cefeeb10309fe
Ubuntu <ubuntu@ip-172-31-41-55.ec2.internal>
2019-10-11 11:42:00 +0000

    Added chat textbox function and refactored code

--- run.py
+++ run.py
@@ -1,34 +1,40 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
+
 
 def add_message(username,message):
     """Add messages to the add messages list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp": now, "from": username, "message": message})
 
-def get_all_messages():
-    """Get all of the messages and seperate using the br"""
-    return"<br>".join(messages)
-
-@app.route("/")
+@app.route("/", methods =["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
     
-@app.route('/<username>')
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+        
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+    
+    return render_template("index.html")
+    
+@app.route('/chat/<username>', methods= ["GET", "POST"])
 def user(username):
-    """ display chat messages """
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    """ Add and display chat messages """
+    
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
+    
+    return render_template("chat.html", username = username, chat_messages = messages)
 
-@app.route('/<username>/<message>')
-def send_message(username, message):
-    """ create a new message and redirect back to chat page"""
-    add_message(username, message)
-    return redirect("/" + username)
-
-if __name__ =="__main__":
-    app.run(host=os.getenv("IP"),
-       port=int(os.getenv("PORT")),
-       debug=True)
+app.run(host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "5000")), debug=False)

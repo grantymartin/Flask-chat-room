c7079c8fb6cd2894eab4bb03a739fa80601ea00a
Ubuntu <ubuntu@ip-172-31-41-55.ec2.internal>
2019-10-11 10:26:10 +0000

    basic functionallity

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
 
+def add_message(username,message):
+    """Add messages to the add messages list"""
+    messages.append("{}: {}".format(username, message))
+
+def get_all_messages():
+    """Get all of the messages and seperate using the br"""
+    return"<br>".join(messages)
 
 @app.route("/")
 def index():
@@ -11,11 +19,14 @@ def index():
     
 @app.route('/<username>')
 def user(username):
-    return "Hi" + username
+    """ display chat messages """
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """ create a new message and redirect back to chat page"""
+    add_message(username, message)
+    return redirect("/" + username)
 
 if __name__ =="__main__":
     app.run(host=os.getenv("IP"),
